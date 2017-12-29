5d389be53d288798894a677588bc2292b2c5ece8
ZombyOS <30983806+ZombyOS@users.noreply.github.com>
2017-12-29 15:09:21 +0100

    Initial commit
    Supported functions:
    Rolling a D6 a number of times 
    Hypergeometric random model
    binomial random model (Bernoulli)
    PQ-Formula
    µ and ∂ of random models
    Square roots
    e^x 
    Calculus for the funtion 1+x^-2
    Vector length and scalar product

---
+++ rechner.py
@@ -0,0 +1,204 @@
+from math import *
+from random import randint
+import numpy as np 
+
+def dice():
+	results = []
+	result_count = {}
+	durch = int(input("Anzahl der Durchgänge:"))
+	for i in range(durch):
+		count = 0
+		for i in range(100):
+			number = 0
+			number = randint(1, 6)
+			if number == 6:
+				count += 1
+		results.append(count)
+	for i in range(100):
+		num = results.count(i)
+		result_count[i] = num
+	for key, value in result_count.items():
+		print(str(key) + " : " + str(value))
+	results.sort()
+	ave = sum(results) / durch
+	print(ave)
+
+def erw():
+	lang = int(input("Geben Sie die Anzahl an Ergebnissen an: "))		#Wie viele mögliche Ergebnisse sind vorhanden?
+	werte = {}
+	ergebnis = []
+	for i in range(lang):
+		ereig = float(input("Zufallswert: "))							#Abfrage der Ergebnisse und der jeweiligen Wahrscheinlichkeit
+		werte[ereig] = float(input("Wahrscheinlichkeit: "))
+
+	for key, value in werte.items():									#Gewichtung durch Multiplikation von Ergebnis und Wahrscheinlichkeit
+		erg = key * value
+		ergebnis.append(erg)
+
+	result = sum(ergebnis)												#Ergebnisse der Gewichtung zusammenrechnen												
+	return werte
+	print(result)
+
+def hypGeo():
+	n = int(input("Spezifizieren sie die Gesamtmenge:"))
+	m = int(input("Spezifizieren Sie die Menge der Treffer:"))
+	z = int(input("Spezifizieren Sie die Anzahl der Ziehungen:"))
+	k = int(input("Spezifizieren Sie die gewünschte Trefferanzahl:"))
+	#Aussortierung aller unmöglichen Ergebnisse
+	if m > n:
+		print("Error: Treffermenge ist größer als Gesamtmenge")
+	elif k > z:
+		print("Error: Gewünschte Trefferanzahl ist größer als die Anzahl der Ziehungen")
+	elif k > m:
+		print("Error: Die gewünschte Trefferzahl ist größer als Treffermenge")
+	elif z > n:
+		print("Error: So viele Ziehungen sind nicht möglich")
+	#Formel
+	else:
+		ol = factorial(m) / (factorial(k) * factorial(m - k))						#Oben links in der Gleichung (M über k)
+		obr = factorial(n - m) / (factorial(z - k) * factorial((n - m) - (z - k)))	#Oben Rechts in der Gleichung (N - M über n - k)
+		un = factorial(n) / (factorial(z) * factorial(n - z))						#Unten in der Gleichung (N über n)
+		result = (ol * obr) / un
+		print(str(result * 100) + "%")
+		print(result)
+
+def pq():
+	p = float(input("Geben Sie p an: "))
+	q = float(input("Geben Sie q an: "))
+	if (p / 2) ** 2 - q < 0:
+		return "Die Formel hat keine Lösung. Damit hat der Graph keine Nullstellen!"
+	else:
+		root = sqrt((p / 2) ** 2 - q)
+		x1 = (p / 2) + root
+		x2 = (p / 2) - root
+
+		print(x1)
+		print(x2)
+
+def bin():
+	n = int(input("Geben Sie die Länge der Kette an: "))
+	k = int(input("Geben Sie die gewünschte Ergebnismenge an: "))
+	p = float(input("Geben Sie die Wahrscheinlichkeit für das gewünschte Ereignis an: "))
+	#Unmögliche Ergebnisse aussortieren
+	if k > n:
+		return "Dies ist nicht möglich. k kann nicht größer sein als die Kettenlänge."
+	elif p >= 1:
+		return "Dies ist nicht möglich. P muss kleiner als 1 sein."
+	#Formel
+	else:
+		q = 1 - p
+		cr = factorial(n) / (factorial(k) * factorial(n - k))
+		result = cr * p ** k * q ** (n - k)
+		print(result)
+
+def sqroot():
+	number = float(input("Zahl: "))
+	result = sqrt(number)
+	print(result)
+
+def e():
+	"""Berechnet entweder e^x oder e^x von 1-10"""
+	try:
+		expo = float(input("Exponent: "))
+	except SyntaxError:
+		expo = None
+	if expo is None:		
+		for i in range(1, 10):
+			result = e ** i
+			print(str(i) + str(result))
+	else: 
+		res = exp(expo)
+		print(res)
+
+def abweichung():
+	"""Berechnet die Standardabweichung"""
+	liste = {}
+	liste = erw()
+	for key, value in liste.items():									#Gewichtung durch Multiplikation von Ergebnis und Wahrscheinlichkeit
+		erg = key * value
+		ergebnis.append(erg)
+	result = sum(ergebnis)
+
+	liste2 = []
+	for key, value in liste.items():
+		first = (key * result) ** 2
+		gesamt = first * value
+		liste2.append(gesamt)
+
+	ergeb = sum(liste2)
+	ergebnis = sqrt(ergeb)
+	print(ergebnis)
+
+def binErw():
+	n = int(input("Geben Sie die Anzahl der Wiederholungen ein: "))
+	p = int(input("Geben Sie die Wahrscheinlichkeit an: "))
+	if p == 1:
+		print(n)
+	elif p > 1:
+		print("p kann nicht größer als 1 sein")
+	else:
+		result = n * p
+		print(result)
+
+def binAbweichung():
+	n = float(input("Geben Sie die Anzahl der Wiederholungen an: "))
+	p = float(input("Geben Sie die Wahrscheinlichkeit an: "))
+	if p > 1:
+		print("p kann nicht größer als 1 sein.")
+	else:
+		var = n * p * (1 - p)
+		result = sqrt(var)
+		print(result)
+
+
+def integral():
+	lowerEnd = float(input("Geben Sie die untere Grenze an:"))
+	upperEnd = float(input("Geben Sie die obere Grenze an:"))
+	stepCount = int(input("Geben sie die Anzahl an Schritten an:"))
+	integralSize = upperEnd - lowerEnd
+	stepSize = integralSize / stepCount
+	fValues = []
+	start = lowerEnd + stepSize
+	end = upperEnd - stepSize
+	while start < end:
+		fValues.append(2 * f(start))
+		start += stepSize
+
+	fValues.append(f(lowerEnd))
+	fValues.append(f(upperEnd))
+	result = ((upperEnd - lowerEnd) / (2 * stepCount)) * sum(fValues) 
+	print(result)
+
+def f(pos):
+	return 1 / (1 + pos ** 2)
+
+def veclen(vector):							#calculates the length of the vector given 
+	vector = vector ** 2
+	result = 0
+	for i in vector:
+		result += sqrt(i)
+	return result
+
+def scaleProduct():							#calculates the scalar product of the two vectors given when running
+	avec = vectorCreation()
+	bvec = vectorCreation()
+
+	alen = veclen(avec)
+	blen = veclen(bvec)
+	result = alen * blen * angle
+	return result
+
+def vectorLength():						#takes the input for a vector and passes it to the veclen() function
+	result = veclen(vectorCreation())
+	return result
+
+def crossProduct():
+	
+def vectorCreation():
+	print("Just use 3-dimensional vectors")
+	veclist = []
+	for i in range(2):
+		veclist.append(float(input("Geben sie die " + str(i + 1) + ". Nummer ein")))
+	vec = np.array(veclist)
+	return vec
+
---
+++ rechnerhaupt.py
@@ -0,0 +1,42 @@
+from rechner import *
+def choose():
+	"""Auswahl der Funktion"""
+	print("1: Würfelversuch")
+	print("2: Hypergeometrische Verteilung")
+	print("3: Binomialverteilung/Bernoulli-Kette")
+	print("4: pq-Formel")
+	print("5: Erwartungswert")
+	print("6: Standardabweichung")
+	print("7: Wurzel ziehen")
+	print("8: e-Funktion")
+	print("9: Binom. Erwartungswert")
+	print("10: Binom. Standardabweichung")
+	print("11: Integralannäherung für 1+x^-2")
+	print("12: Vektorlänge")
+	print("13: Skalarprodukt")
+	options = {1: dice,
+	2: hypGeo,
+	3: bin,
+	4: pq,
+	5: erw,
+	6: abweichung,
+	7: sqroot,
+	8: e,
+	9: binErw,
+	10: binAbweichung,
+	11: integral,
+	12: vectorLength,
+	13: scaleProduct}
+	num = int(input("Welche Funktion soll ausgeführt werden? "))
+	options[num]()
+
+
+while True:
+
+	choose()
+
+	again = input("Wollen Sie weitermachen? [Y/N]: ")
+	if again.lower() == "y":
+		continue
+	elif again.lower() == "n":
+		quit() 
